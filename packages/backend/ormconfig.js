4dd47d58007743e17726e1f5293425370037ed87
biubiu52011 <junyan809966093@gmail.com>
2025-04-27 01:22:52 +0900

    Update ormconfig.js

--- packages/backend/ormconfig.js
+++ packages/backend/ormconfig.js
@@ -1,16 +1,16 @@
-module.exports = {
-    type: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    username: process.env.DB_USER || 'misskey',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'misskey',
-    synchronize: false,
-    logging: false,
-    entities: ['dist/entities/**/*.js'],
-    migrations: ['dist/migrations/**/*.js'],
-    cli: {
-        entitiesDir: 'src/entities',
-        migrationsDir: 'src/migrations'
-    }
+export default {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USER || 'misskey',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'misskey',
+  synchronize: false,
+  logging: false,
+  entities: ['dist/entities/**/*.js'],
+  migrations: ['dist/migrations/**/*.js'],
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations'
+  }
 };
